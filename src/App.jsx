a76c1233f04af1dbc44d253fe71f262e5febf173
Vaishnavi-cloud321 <ch.vaishnavi361214@gmail.com>
2026-03-01 22:47:58 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Weather from './components/Weather';
+import Myapp from './components/Myapp';
+
+function App() {
+  return (
+    <>
+     
+      <Myapp/>
+    </>
+  );
+}
+
+export default App;
